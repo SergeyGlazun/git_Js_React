ebe7101a0a811aa6306b29eee6d98e2235bcf495
Vasiliy Pupkin <vpupkin@company.com>
2022-01-19 13:53:19 +0300

    add conectionSaler

--- js/script.js
+++ js/script.js
@@ -35,63 +35,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Timer
     
-    // const deadline = '2020-05-11';
-
-    // function getTimeRemaining(endtime) {
-    //     const t = Date.parse(endtime) - Date.parse(new Date()),
-    //         days = Math.floor(t / (1000 * 60 * 60 * 24)),
-    //         hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-    //         minutes = Math.floor((t / 1000 / 60) % 60),
-    //         seconds = Math.floor((t / 1000) % 60);
-
-    //     return {
-    //         'total': t,
-    //         'days': days,
-    //         'hours': hours,
-    //         'minutes': minutes,
-    //         'seconds': seconds
-    //     };
-    // }
-
-    // function getZero(num) {
-    //     if (num >= 0 && num < 10) {
-    //         return `0${num}`;
-    //     } else {
-    //         return num;
-    //     }
-    // }
-
-
-    // function setClock(selector, endtime) {
-        
-    //     const timer = document.querySelector(selector),
-    //         days = timer.querySelector('#days'),
-    //         hours = timer.querySelector('#hours'),
-    //         minutes = timer.querySelector('#minutes'),
-    //         seconds = timer.querySelector('#seconds'),
-    //         timeInterval = setInterval(updateClock, 1000);
-
-    //     updateClock();
-
-    //     function updateClock() {
-    //         const t = getTimeRemaining(endtime);
-
-    //         days.innerHTML = getZero(t.days);
-    //         hours.innerHTML = getZero(t.hours);
-    //         minutes.innerHTML = getZero(t.minutes);
-    //         seconds.innerHTML = getZero(t.seconds);
-
-    //         if (t.total <= 0) {
-    //             clearInterval(timeInterval);
-    //         }
-    //     }
-    // }
-
-    // setClock('.timer', deadline);
-
-    //   const deadline = '2022-05-11';
       const deadline = '2022-05-11';
 
     function getTimeRemaining(endtime) {
@@ -144,4 +88,23 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadline);
+
+    const modalTrigger = document.querySelectorAll('.btn_connection'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('.modal__close');
+
+
+    modalTrigger.forEach((item,i) =>{
+        item.addEventListener('click', () => {         
+            modal.style.display = "block";     
+            document.body.style.overflow = 'hidden';         
+        });
+      
+    })
+
+    modalCloseBtn.addEventListener('click', ()=>{
+        modal.style.display = "none";   
+        document.body.style.overflow = '';     
+});
+
 });
